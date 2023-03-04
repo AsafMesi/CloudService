0bae21cabc123f4268cefb9afc663eae55018419
Asaf Mesilaty <92261832+AsafMesi@users.noreply.github.com>
2023-03-04 22:46:18 +0200

    Delete testForCommand.py

--- testForCommand.py
+++
@@ -1,8 +0,0 @@
-import os
-
-file_path = "C:\\Users\\asaf4\\OneDrive\\Desktop\\ClientFolder\\Instructions.txt"
-
-dir_name = os.path.dirname(file_path)
-print(dir_name)     # output: C:\Users\asaf4\OneDrive\Desktop\ClientFolder
-base_name = os.path.basename(file_path)
-print(base_name)    # output: Instructions.txt
